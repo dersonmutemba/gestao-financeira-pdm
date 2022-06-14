21a353d11e45f5150d74cc374914df2426495592
dersonmutemba <dersonmutemba@gmail.com>
2022-06-14 06:53:11 -0500

    Login screen

--- app/src/main/java/mz/ac/isutc/gestaofinanceira/Login.java
+++ app/src/main/java/mz/ac/isutc/gestaofinanceira/Login.java
@@ -2,7 +2,15 @@ package mz.ac.isutc.gestaofinanceira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Stack;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void entrar(View view) {
+        EditText editTextUsername = findViewById(R.id.usernameEditTextLogin);
+        EditText editTextPassword = findViewById(R.id.passwordEditTextLogin);
+
+        SharedPreferences preferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        String username = preferences.getString("Username", "/");
+        Log.i("Info", username);
+
+
     }
 }
