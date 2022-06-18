9e3b4ec4c49d8d08f2059d20ba78ebd959ad3278
Khalidy Ibraimo Safar <88678823+Khalidy6@users.noreply.github.com>
2022-06-18 10:42:58 +0300

    Add files via upload

--- java/mz/ac/isutc/gestaofinanceira/Definicoes.java
+++ java/mz/ac/isutc/gestaofinanceira/Definicoes.java
@@ -19,7 +19,8 @@ import android.widget.Toast;
  * Use the {@link Definicoes#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Definicoes extends Fragment {
+
+public class Definicoes extends Fragment implements AdapterView.OnItemSelectedListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,7 +34,6 @@ public class Definicoes extends Fragment {
     public Definicoes() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -46,6 +46,7 @@ public class Definicoes extends Fragment {
 
     Spinner spinner;
     Button btnPerfil;
+    ArrayAdapter adpt;
     private String [] moedas = {"Metical MZN", "Rand RND", "Euro EUR", "Dollar DLR"};
 
     public static Definicoes newInstance(String param1, String param2) {
@@ -58,48 +59,39 @@ public class Definicoes extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        spinner = findViewById(R.id.spinnerMoedas);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        spinner = getView().findViewById(R.id.spinnerMoedas);
         spinner.setOnItemSelectedListener(this);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-        ArrayAdapter adpt = new ArrayAdapter(this, android.R.layout.simple_spinner_item, moedas);
+        adpt = new ArrayAdapter(getContext(),android.R.layout.simple_spinner_item, moedas);
         adpt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adpt);
 
-        btnPerfil = findViewById(R.id.btPerfil);
+        btnPerfil = getView().findViewById(R.id.btPerfil);
         btnPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openNewActivity();
             }
         });
+
+        return inflater.inflate(R.layout.fragment_definicoes, container, false);
     }
-    public void openNewActivity(){
-        Intent intent = new Intent(this, Perfil.class);
+
+    private void openNewActivity(){
+        Intent intent = new Intent(getContext(), Perfil.class);
         startActivity(intent);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, moedas[position],Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), moedas[position],Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_definicoes, container, false);
     }
 }
